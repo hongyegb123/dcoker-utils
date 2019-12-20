688d4233ba5fa7d4f90e7512e3ade0a5563dfe9e
hongyegb123 <hongyegb123@163.com>
2019-12-20 11:57:41 +0800

    Update install_docker-compose.sh

--- install_docker-compose.sh
+++ install_docker-compose.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-$COMPOSE_VERSION="1.25.0"
+COMPOSE_VERSION="1.25.0"
+COMPOSE_VERSION="master"
 
 curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-curl -L https://raw.githubusercontent.com/docker/compose/1.25.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose${COMPOSE_VERSION}
+curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose${COMPOSE_VERSION}
