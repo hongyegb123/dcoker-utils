2dcf80fe368a609bf5074bb8f6f58ea115a1bf18
binbin <hongyegb123@163.com>
2020-08-05 13:31:31 +0800

    Update install_docker-ce.sh

--- install_docker-ce.sh
+++ install_docker-ce.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Docker remove <none> TAG images (https://stackoverflow.com/questions/33913020/docker-remove-none-tag-images)
+# How to remove old and unused Docker images (https://stackoverflow.com/a/32723127/6309)
+# Docker remove <none> TAG images
+# docker image prune
+# docker images -q -a | xargs docker inspect --format='{{.Id}}{{range $rt := .RepoTags}} {{$rt}} {{end}}'|grep -v ':'
+# docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+
+
 yum install -y yum-utils
 
 yum-config-manager \
